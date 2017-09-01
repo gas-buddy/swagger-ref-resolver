230dfbab9b864a2b6dc9a76fc36942e9df8e1be7
Max Metral <max@pyralis.com>
2017-09-01 10:10:13 -0400

    Add support for validation in CLI via --validate arg after source filename

--- src/swagger-pack.js
+++ src/swagger-pack.js
@@ -1,12 +1,31 @@
 #!/usr/bin/env node
+import { Readable } from 'stream';
+import validator from 'swagger-spec-validator';
 import jsonResolver from './index';
 
 const sourceFile = process.argv[2];
 
+function pretty(obj) {
+  return JSON.stringify(obj, null, '\t');
+}
+
 jsonResolver(sourceFile)
+  .then(async (doc) => {
+    if (process.argv[3] === '--validate') {
+      const r = new Readable();
+      r.push(pretty(doc));
+      r.push(null);
+      const result = await validator.validate(r);
+      if (Object.keys(result).length > 0) {
+        throw new Error(`Validation Failed!
+${pretty(result)}`);
+      }
+    }
+    return doc;
+  })
   .then((doc) => {
     // eslint-disable-next-line no-console
-    console.log(JSON.stringify(doc, null, '\t'));
+    console.log(pretty(doc));
   }).catch((error) => {
     // eslint-disable-next-line no-console
     console.error(error.message, error.stack);
